9585c7e28230de95f790b8fa3ee026d927b2371b
manandre <2341261+manandre@users.noreply.github.com>
2019-08-17 13:41:20 +0200

    Adapt source tree for unit tests

---
+++ FollowingFileStream.Tests/FollowingFileStreamTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FollowingFileStream.Test
+{
+    [TestClass]
+    public class FollowingFileStreamTest
+    {
+        //private readonly FollowingFileStream ffs;
+
+
+        [TestMethod]
+        public void FFS_First()
+        {
+            Assert.IsTrue(true, "should be true");
+        }
+    }
+}
