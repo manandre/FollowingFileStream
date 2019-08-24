2b2e76eab6ece220179c72592002ed22ee399ecd
manandre <2341261+manandre@users.noreply.github.com>
2019-08-24 23:25:33 +0200

    Create intermediate abstarct AsyncStream

--- FollowingFileStream.Tests/FollowingFileStreamTest.cs
+++ FollowingFileStream.Tests/FollowingFileStreamTest.cs
@@ -73,7 +73,6 @@ namespace FollowingFileStream.Tests
         [DataTestMethod]
         [DataRow(false)]
         [DataRow(true)]
-        [TestMethod]
         public void FFS_Properties(bool async)
         {
             using (var ffs = new FollowingFileStream(inputFilePath, 4*1096, async))
