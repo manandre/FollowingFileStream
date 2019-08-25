398d61376084ce824ac4fdcb6a8f93d2c5e7cc27
manandre <2341261+manandre@users.noreply.github.com>
2019-08-25 11:12:11 +0200

    Create AsyncStream.cs

---
+++ FollowingFileStream/AsyncLock.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FollowingFileStream
+{
+    public sealed class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        public async Task<AsyncLock> LockAsync()
+        {
+            await _semaphoreSlim.WaitAsync();
+            return this;
+        }
+
+        public AsyncLock Lock()
+        {
+            return LockAsync().GetAwaiter().GetResult();
+        }
+
+        public void Dispose()
+        {
+            _semaphoreSlim.Release();
+        }
+    }
+}
